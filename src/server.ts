c00fbdd61c82bddbfb25b85ccb1677c424a8415a
Mohamed Kouache <mkouache27@gmail.com>
2023-11-06 01:48:52 +0100

    route handling

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import { createServer } from "net";
+import { Socket, createServer } from "net";
 
 
 
@@ -7,13 +7,53 @@ const server = createServer()
 server.on("connection", handleTcpConnection)
 server.listen(3000, () => console.log("listening on port : ", server.address()))
 
-function handleTcpConnection(cx: any){
+function handleTcpConnection(cx: Socket){
 
     let remoteAddress = cx.remoteAddress + ':' + cx.remotePort;  
     console.log('new client connection from %s', remoteAddress);
 
-    cx.on("data", (data: any) => {
-        console.log("data : ", data)
+    cx.setEncoding("utf8")
+
+    cx.on("data", (data: String) => onRequest(data, cx))
+
+    cx.on("close", () => {
+        console.log("connection is closed from : ", remoteAddress)
     })
+
+    cx.on("error", (error: Object) => {
+        console.log("error : ", error)
+    })
+}
+
+function onRequest(data: String, cx: Socket) {
+    const url = getBaseUrl(data)
+    if (url === '/') {
+        cx.write(parseResponse('Welcome to the homepage!'));
+    } else if (url === '/about') {
+        cx.write(parseResponse('About us page'));
+    } else {
+        cx.write(parseResponse('Page not found'));
+    }
+}
+
+function parseResponse (data: String) {
+    const httpRespose = [
+        "HTTP/1.1 200 OK",
+        "Content-Type: text/html",
+        `Content-length: ${byteSize(data)}`,
+        "",
+        "",
+        data,
+    ].map(line => line.concat("\r\n")).join('')
+    return httpRespose
+}
+
+function getBaseUrl(incomingMessage: String): String {
+    const requestObject = incomingMessage.split('\r\n')
+    return requestObject[0].split(' ')[1]
+}
+
+function byteSize (data: any) {
+    return new Blob([data]).size
 }
 
